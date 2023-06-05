e67e971fe945b72bd1c5c6a54c6355b554b4dc11
Leedagee <61650578+leedagee@users.noreply.github.com>
2023-06-05 23:43:21 +0800

    some vtune settings maybe

--- src/SpMM_opt.cc
+++ src/SpMM_opt.cc
@@ -1,29 +1,43 @@
 #include "SpMM.hh"
 #include "leedagee/CSR.hh"
+#include <ittnotify.h>
 
 using namespace leedagee;
 
 Matrix SpMM_opt(const Matrix &A, const SparseMatrix &B) {
+  auto res = Matrix(0, 0);
+#ifdef VTUNE_ENABLE
+  __itt_resume();
+  for (int __count = 0; __count < 256; __count++) {
+#endif
   // A is m*k, B is n*k, C is m*n
   auto [m, k] = A.size();
   auto [n, _k] = B.size();
-  std::vector<float> aT(m * k);
+  std::vector<float> _aT(m * k);
+  auto aT = _aT.data();
   for (int i = 0; i < m; i++)
     for (int j = 0; j < k; j++) {
       aT[j * m + i] = A.at(i, j);
     }
-  std::vector<float> cT(m * n);
+  std::vector<float> _cT(m * n);
+  auto cT = _cT.data();
   auto bS = fromSparseMatrix(B);
   for (const auto &elem : bS.d) {
     const auto &r = elem.x, &c = elem.y;
     for (int i = 0; i < m; i++)
       cT[r * m + i] += elem.v * aT[c * m + i];
   }
-  std::vector<float> c;
+  std::vector<float> _c(m * n);
+  auto c = _c.data();
   for (int i = 0; i < m; i++) {
     for (int j = 0; j < n; j++) {
-      c.push_back(cT[j * m + i]);
+      c[i * n + j] = cT[j * m + i];
     }
   }
-  return Matrix(c, m, n);
+  res = std::move(Matrix(_c, m, n));
+#ifdef VTUNE_ENABLE
+  }
+  __itt_pause();
+#endif
+  return res;
 }
